5439aafc0c01c680bb7828dfd673596839b2d3a0
pretro <jort2212@outlook.com>
2023-09-11 19:35:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, computed } from 'vue'
+const reviews = ref(googleReviewsData.count)
+const average = computed(() => {
+  return googleReviewsData.averageScore.toFixed(1);
+})
+
+const getLang = computed(() => {
+  return document.querySelector('html').getAttribute('lang')
+})
+
+</script>
+
+<template>
+  <div>
+    <div>
+      <p>{{ average }} of 5.0</p>
+      <p>{{ reviews }} Customer ratings</p>
+    </div>
+  </div>
+</template>
